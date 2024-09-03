9c861a860056b5ec553a99746f3af86c16eec30c
Miha Filej <miha@filej.net>
2024-09-03 15:29:41 +0200

    Upgrade to phoenix_html 4

--- lib/ridle_web.ex
+++ lib/ridle_web.ex
@@ -84,8 +84,9 @@ defmodule RidleWeb do
 
   defp view_helpers do
     quote do
-      # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
+      import Phoenix.HTML
+      import Phoenix.HTML.Form
+      use PhoenixHTMLHelpers
 
       alias Phoenix.LiveView.JS
       import Phoenix.LiveView.Helpers
