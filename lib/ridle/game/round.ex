ef7b3581244f89ca7f995073e8e701ddd466404c
Miha Filej <miha@filej.net>
2024-11-10 15:20:31 +0100

    Remove reference to starts_at

--- lib/ridle/game/round.ex
+++ lib/ridle/game/round.ex
@@ -8,7 +8,6 @@ defmodule Ridle.Game.Round do
     field :year_start, :integer
     field :year_end, :integer
     field :image_url, :string
-    field :starts_at, :utc_datetime
 
     timestamps()
   end
@@ -16,7 +15,7 @@ defmodule Ridle.Game.Round do
   @doc false
   def changeset(round, attrs) do
     round
-    |> cast(attrs, [:make, :model, :year_start, :year_end, :image_url, :starts_at])
-    |> validate_required([:make, :model, :year_start, :year_end, :image_url, :starts_at])
+    |> cast(attrs, [:make, :model, :year_start, :year_end, :image_url])
+    |> validate_required([:make, :model, :year_start, :year_end, :image_url])
   end
 end
