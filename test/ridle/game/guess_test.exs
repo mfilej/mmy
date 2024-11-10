1d23657e5913d4e015861b4204d20bcc50b8bb1c
Miha Filej <miha@filej.net>
2024-11-10 15:12:23 +0100

    Clean up tests that don't do what we want anymore

--- test/ridle/game/guess_test.exs
+++ test/ridle/game/guess_test.exs
@@ -1,13 +1,5 @@
 defmodule Ridle.Game.GuessTest do
   use Ridle.DataCase, async: true
 
-  alias Ridle.Game.Guess
-
-  test "normalizes make and model" do
-    assert %Ecto.Changeset{changes: %{make: "astonmartin", model: "db9"}} =
-             Guess.changeset(%Guess{}, %{make: "Aston Martin", model: "DB 9"})
-
-    assert %Ecto.Changeset{changes: %{make: "rollsroyce", model: "phantom"}} =
-             Guess.changeset(%Guess{}, %{make: "Rolls-Royce", model: "Phantom"})
-  end
+  alias Ridle.Game.Guess, warn: false
 end
--- test/ridle/game_test.exs
+++ test/ridle/game_test.exs
@@ -3,25 +3,6 @@ defmodule Ridle.GameTest do
 
   alias Ridle.Game
 
-  test "returns today's round" do
-    insert_round(~U[2022-04-14 00:00:00Z], "Renault", "Twingo", 1993)
-    insert_round(~U[2022-04-15 00:00:00Z], "Opel", "Omega", 1990)
-    insert_round(~U[2022-04-16 00:00:00Z], "Ford", "Capri", 1978)
-
-    now = ~U[2022-04-15 22:55:00Z]
-
-    assert %Game.Round{make: "Opel", model: "Omega", year_start: 1990} = Game.current_round(now)
-  end
-
-  test "returns newest round that is not in the future" do
-    insert_round(~U[2022-04-14 00:00:00Z], "Renault", "Twingo", 1993)
-    insert_round(~U[2022-04-16 00:00:00Z], "Ford", "Capri", 1978)
-
-    now = ~U[2022-04-15 22:55:00Z]
-
-    assert %Game.Round{make: "Renault", model: "Twingo", year_start: 1993} = Game.current_round(now)
-  end
-
   defp insert_round(starts_at, make, model, year) do
     {:ok, _} =
       %Game.Round{
