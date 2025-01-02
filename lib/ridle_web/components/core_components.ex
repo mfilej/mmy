207fae8425c9a9bba2839fc2aeb473d4963386cd
Miha Filej <miha@filej.net>
2025-01-02 10:53:39 +0100

    Hide validation messages

--- lib/ridle_web/components/core_components.ex
+++ lib/ridle_web/components/core_components.ex
@@ -402,7 +402,7 @@ defmodule RidleWeb.CoreComponents do
 
   def error(assigns) do
     ~H"""
-    <p class="mt-3 flex gap-3 text-sm leading-6 text-rose-600">
+    <p class="sr-only mt-3 flex gap-3 text-sm leading-6 text-rose-600">
       <.icon name="hero-exclamation-circle-mini" class="mt-0.5 h-5 w-5 flex-none" />
       <%= render_slot(@inner_block) %>
     </p>
